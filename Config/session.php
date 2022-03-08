1203a7b36e7f1711e7d158d2e84029bdb0ee687c
kyour-cn <kyour@outlook.com>
2022-03-08 23:25:56 +0800

    更新Session配置项

--- Config/session.php
+++ Config/session.php
@@ -6,5 +6,8 @@ return [
     'enable'      => true, //启用Session
     'type'        => 'file', //储存驱动 redis、file
     'session_name' => 'sessionId', //Session的CookieName
-    'expire'      => 86400 * 7 //过期时间 s
+    'expire'      => 3600 * 24, //过期时间 s
+    //SessionId获取优先级：header>参数>cookie
+    'enable_header' => false, //是否支持Header中获取SessionID
+    'enable_param' => false //是否支持从请求参数中获取SessionID
 ];
